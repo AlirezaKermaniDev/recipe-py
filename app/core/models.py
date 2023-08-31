c75695afaffe14c7d085d9204e05c91b98724fdd
alireza kermani <alirezakermani.dev@gmail.com>
2023-08-31 13:07:33 +0330

    Added admin interface and user model and test.

--- app/core/models.py
+++ app/core/models.py
@@ -8,17 +8,30 @@ from django.contrib.auth.models import (
 
 class UserManager(BaseUserManager):
 
-    def create_user(self, email, password=None, **extera_field):
-        user = self.model(email=email, **extera_field)
+    def create_user(self, email, password=None, **extra_fields):
+
+        if not email:
+            raise ValueError("Email address cannot be none or empty")
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
+    def create_superuser(self, email, password):
 
-class USer(AbstractBaseUser, PermissionsMixin):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+
     email = models.EmailField(max_length=255, unique=True)
-    name = models.CharField(max_length=255),
+    name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -13,3 +13,32 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_normalizr_email_address(self):
+        sampleEmail = [
+            ["test1@EXAMPLE.com", "test1@example.com"],
+            ["Test2@Example.com", "Test2@example.com"],
+            ["TEST3@EXAMPLE.COM", "TEST3@example.com"],
+            ["test4@example.COM", "test4@example.com"],
+        ]
+
+        for email, excepted in sampleEmail:
+            user = get_user_model().objects.create_user(email, "123456")
+            self.assertEqual(user.email, excepted)
+
+    def test_raise_error_when_email_is_blank(self):
+
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(
+                "",
+                "123456"
+            )
+
+    def test_create_superuser(self):
+
+        user = get_user_model().objects.create_superuser(
+            'text@example.com', "123456",
+            )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
