f74ce92b158e72035fd59ab34ebeaf960dc69baa
alireza kermani <alirezakermani.dev@gmail.com>
2023-09-03 10:06:58 +0330

    Added user/me api  and recipe app

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from decimal import Decimal
+from core import models
 
 
 class ModelTests(TestCase):
@@ -38,7 +40,24 @@ class ModelTests(TestCase):
 
         user = get_user_model().objects.create_superuser(
             'text@example.com', "123456",
-            )
+        )
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_create_recipe(self):
+
+        user = get_user_model().objects.create_user(
+            'test@gmail.com',
+            'testpass222',
+        )
+
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title='Simple food name',
+            time_minutes=5,
+            price=Decimal('5.50'),
+            description='Simple recipe description',
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
